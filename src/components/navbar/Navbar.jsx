af144f45602245f0fa3c2474455c20f2b6312540
Sahy <lianasandramahery@gmail.com>
2023-08-12 00:16:00 +0300

    Payment user information implemented

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { BiSearch } from "react-icons/bi";
 import "./navbar.scss";
 import Cart from "./Cart";
 import Axios from "axios";
+import Modal from "react-modal";
 import { useSnackbar, enqueueSnackbar } from "notistack";
 
 function Navbar({
@@ -24,8 +25,38 @@ function Navbar({
   const menuEl = React.useRef();
   const [loading, setLoading] = React.useState(false);
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [myDetails, setMyDetails] = React.useState({});
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    document.body.style.overflow = "hidden";
+    subtitle.style.color = "blue";
+  }
+
+  function closeModal() {
+    document.body.style.overflow = "scroll";
+    setIsOpen(false);
+  }
 
   // const [total, setTotal] = React.useState();
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%,-50%)",
+      width: "60%",
+      height: "75vh",
+      // overflowY: "scroll",
+    },
+  };
 
   let myTotal = () => {
     let getTotal = 0;
@@ -112,32 +143,85 @@ function Navbar({
   const handlePayment = async (e) => {
     e.preventDefault();
 
-    setLoading(true);
+    // setLoading(true);
 
     console.log(cartElts);
 
     if (cartElts.length > 0) {
-      try {
-        const buyProduct = await Axios.patch(
-          "http://localhost:3000/api/products/payments",
-          {
-            myCarts: cartElts,
-          }
-        );
-        enqueueSnackbar("Payment succeeded", {
-          variant: "success",
-        });
-        setCartElts([]);
-      } catch (error) {
-        enqueueSnackbar(`Payment failed!!!${error.response.data.msg}`, {
-          variant: "error",
-        });
-        // console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    } else {
+      //try {
+      // const buyProduct = await Axios.patch(
+      //   "http://localhost:3000/api/products/payments",
+      //   {
+      //     myCarts: cartElts,
+      //   }
+      // );
+      // enqueueSnackbar("Payment succeeded", {
+      //   variant: "success",
+      // });
+      // setCartElts([]);
+      setShowMenu(false);
+      openModal();
+      //} catch (error) {
+      //enqueueSnackbar(`Payment failed!!!${error.response.data.msg}`, {
+      //variant: "error",
+      //});
+      // console.log(error);
+    }
+    //finally {
+    // setLoading(false);
+    // }
+    // }
+    else {
       enqueueSnackbar("Your cart is empty", { variant: "error" });
+      setLoading(false);
+    }
+  };
+
+  Modal.setAppElement("#root");
+
+  const modalCarts = cartElts.map((cart, key) => {
+    return (
+      <tr key={key}>
+        <td>
+          <img src={`http://localhost:3000/products/${cart.image}`} />
+        </td>
+        <td>{cart.name}</td>
+        <td>{cart.nb}</td>
+        <td>{cart.price}</td>
+        <td style={{ fontWeight: "bold" }}>{cart.indTotal}</td>
+      </tr>
+    );
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setMyDetails((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmitPayment = async (e) => {
+    e.preventDefault();
+    console.log("My details are");
+    console.log(myDetails);
+    setLoading(true);
+    try {
+      const buyProduct = await Axios.patch(
+        "http://localhost:3000/api/products/payments",
+        {
+          myCarts: cartElts,
+          myDetails: myDetails,
+        }
+      );
+      enqueueSnackbar("Payment success", {
+        variant: "success",
+      });
+    } catch (error) {
+      enqueueSnackbar(`Payment failed!!!}`, {
+        variant: "error",
+      });
+    } finally {
+      setCartElts([]);
+      setIsOpen(false);
       setLoading(false);
     }
   };
@@ -246,12 +330,105 @@ function Navbar({
 
           <div className="menu-footer">
             {/* <button className="menu-continue">Continue Shopping</button> */}
-            <button className={`menu-pay ${loading ? "loading" : null}`}>
+            {/* <button className={`menu-pay ${loading ? "loading" : null}`}>
               {loading ? "Please wait..." : "Make Payment"}
-            </button>
+            </button> */}
+            <button className={`menu-pay `}>Make payment</button>
           </div>
         </form>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example"
+        // className="modal-container"
+        overlayClassName="overlay"
+      >
+        <form onSubmit={handleSubmitPayment} className="modal-container">
+          <div className="modal-header">
+            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
+              Payement details:
+            </h2>
+            <button className="close" onClick={closeModal}>
+              x
+            </button>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Product image</th>
+                <th>Product name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>{modalCarts}</tbody>
+            <tfoot>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td
+                  style={{
+                    fontWeight: "bold",
+                    color: "blue",
+                    fontSize: "20px",
+                    padding: "10px 0",
+                  }}
+                >
+                  ${total}
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+          {/* {modalCarts} */}
+          {/* <p style={{ textAlign: "right" }}>Total:${total}</p> */}
+          <h2>Your details:</h2>
+          <label htmlFor="name">
+            Name<sup style={{ color: "red" }}>*</sup>:
+          </label>
+          <br />
+          <input
+            onChange={handleChange}
+            name="name"
+            required
+            type="text"
+            id="name"
+          />
+          <br />
+          <label htmlFor="phone">
+            Phone number<sup style={{ color: "red" }}>*</sup>:
+          </label>
+          <br />
+          <input
+            onChange={handleChange}
+            name="phone"
+            required
+            type="text"
+            id="phone"
+          />
+          <br />
+          <label htmlFor="address">
+            Address<sup style={{ color: "red" }}>*</sup>::
+          </label>
+          <br />
+          <input
+            onChange={handleChange}
+            name="address"
+            required
+            type="text"
+            id="address"
+          />
+          <br />
+          <button className={`confirm ${loading && "loading"}`}>
+            {loading ? "Please wait" : "Confirm payment"}
+          </button>
+        </form>
+      </Modal>
     </>
   );
 }
